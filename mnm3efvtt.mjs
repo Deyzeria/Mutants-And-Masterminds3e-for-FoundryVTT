bb163ad3d1f9f3db15a01095dd29c698332c7c5a
Viktors Bardisevs <viktors.bardisevs@knowledgeprice.com>
2024-02-15 15:41:00 +0200

    added scales to autotranslation

--- mnm3efvtt.mjs
+++ mnm3efvtt.mjs
@@ -15,13 +15,27 @@ Hooks.once("i18nInit", function () {
   ];
 
   for (let c of toLocalize) {
+    const conf = foundry.utils.getProperty(SYSTEM, c);
     for ( let [k, v] of Object.entries(conf) ) {
       if ( v.label ) v.label = game.i18n.localize(v.label);
       if ( v.abbreviation) v.abbreviation = game.i18n.localize(v.abbreviation);
       if ( typeof v === "string" ) conf[k] = game.i18n.localize(v);
     }
   }
-  
+
+  const scaleLocalisation = [
+    "DISTANCE", "MASS", "TIME", "VOLUME"
+  ]
+
+  for (let c of scaleLocalisation) {
+    const conf = foundry.utils.getProperty(SYSTEM, c);
+    for ( let [k, v] of Object.entries(conf) ) {
+      if ( v.m_scale) v.m_scale = game.i18n.localize(v.m_scale);
+      if ( v.im_scale) v.im_scale = game.i18n.localize(v.im_scale);
+      if ( v.time_scale) v.time_scale = game.i18n.localize(v.time_scale);
+    }
+  }
+
   // Preload Handlebars Templates
   loadTemplates([
   ]);

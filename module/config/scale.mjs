22aa51b338fe59debf71a0e34cdd1a2a67667674
Viktors Bardisevs <viktors.bardisevs@knowledgeprice.com>
2024-02-15 12:35:26 +0200

    scale updates, adding time displayed and up to 1 day in seconds

--- module/config/scale.mjs
+++ module/config/scale.mjs
@@ -315,48 +315,48 @@ export const MASS = {
     m_label: "750",
     m_scale: "SCALE.G",
     im_value: 1.5,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "1.5",
+    im_scale: "SCALE.LB"
   },
   '-4' : {
     m_value: 1.5,
     m_label: "1.5",
     m_scale: "SCALE.KG",
     im_value: 3,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "3",
+    im_scale: "SCALE.LBs"
   },
   '-3' : {
     m_value: 3,
     m_label: "3",
     m_scale: "SCALE.KG",
     im_value: 6,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "6",
+    im_scale: "SCALE.LBs"
   },
   '-2' : {
     m_value: 6,
     m_label: "6",
     m_scale: "SCALE.KG",
     im_value: 12,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "12",
+    im_scale: "SCALE.LBs"
   },
   '-1' : {
     m_value: 12,
     m_label: "12",
     m_scale: "SCALE.KG",
     im_value: 25,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "25",
+    im_scale: "SCALE.LBs"
   },
   '0' : {
     m_value: 24,
     m_label: "24",
     m_scale: "SCALE.KG",
     im_value: 50,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "50",
+    im_scale: "SCALE.LBs"
   },
   '1' : {
     m_value: 50,
@@ -364,238 +364,426 @@ export const MASS = {
     m_scale: "SCALE.KG",
     im_value: 100,
     im_label: "",
-    im_scale: "SCALE."
+    im_scale: "SCALE.LBs"
   },
   '2' : {
     m_value: 100,
     m_label: "100",
     m_scale: "SCALE.KG",
     im_value: 200,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "200",
+    im_scale: "SCALE.LBs"
   },
   '3' : {
     m_value: 200,
     m_label: "200",
     m_scale: "SCALE.KG",
     im_value: 400,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "400",
+    im_scale: "SCALE.LBs"
   },
   '4' : {
     m_value: 400,
     m_label: "400",
     m_scale: "SCALE.KG",
     im_value: 800,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "800",
+    im_scale: "SCALE.LBs"
   },
   '5' : {
     m_value: 800,
     m_label: "800",
     m_scale: "SCALE.KG",
     im_value: 1600,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "1,600",
+    im_scale: "SCALE.LBs"
   },
   '6' : {
     m_value: 1600,
     m_label: "1,600",
     m_scale: "SCALE.KG",
     im_value: 3200,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "3,200",
+    im_scale: "SCALE.LBs"
   },
   '7' : {
     m_value: 3200,
     m_label: "3.2",
     m_scale: "SCALE.T",
     im_value: 6000,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "3",
+    im_scale: "SCALE.T"
   },
   '8' : {
     m_value: 6000,
     m_label: "6",
     m_scale: "SCALE.T",
     im_value: 12000,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "6",
+    im_scale: "SCALE.T"
   },
   '9' : {
     m_value: 12000,
     m_label: "12",
     m_scale: "SCALE.T",
     im_value: 24000,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "12",
+    im_scale: "SCALE.T"
   },
   '10' : {
     m_value: 25000,
     m_label: "25",
     m_scale: "SCALE.T",
     im_value: 50000,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "25",
+    im_scale: "SCALE.T"
   },
   '11' : {
     m_value: 50000,
     m_label: "50",
     m_scale: "SCALE.T",
     im_value: 100000,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "50",
+    im_scale: "SCALE.T"
   },
   '12' : {
     m_value: 100000,
     m_label: "100",
     m_scale: "SCALE.T",
     im_value: 200000,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "100",
+    im_scale: "SCALE.T"
   },
   '13' : {
     m_value: 200000,
     m_label: "200",
     m_scale: "SCALE.T",
     im_value: 400000,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "200",
+    im_scale: "SCALE.T"
   },
   '14' : {
     m_value: 400000,
     m_label: "400",
     m_scale: "SCALE.T",
     im_value: 800000,
-    im_label: "",
-    im_scale: "SCALE."
+    im_label: "400",
+    im_scale: "SCALE.T"
   },
   '15' : {
     m_value: 800000,
     m_label: "800",
     m_scale: "SCALE.T",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 1600000,
+    im_label: "800",
+    im_scale: "SCALE.T"
   },
   '16' : {
     m_value: 1600000,
     m_label: "1,600",
     m_scale: "SCALE.T",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 3200000,
+    im_label: "1,600",
+    im_scale: "SCALE.T"
   },
   '17' : {
     m_value: 3200000,
     m_label: "3.2",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 6400000,
+    im_label: "3.2",
+    im_scale: "SCALE.KT"
   },
   '18' : {
     m_value: 600000,
     m_label: "6",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 12000000,
+    im_label: "6",
+    im_scale: "SCALE.KT"
   },
   '19' : {
     m_value: 1200000,
     m_label: "12",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 2400000,
+    im_label: "12",
+    im_scale: "SCALE.KT"
   },
   '20' : {
     m_value: 2500000,
     m_label: "25",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 5000000,
+    im_label: "25",
+    im_scale: "SCALE.KT"
   },
   '21' : {
     m_value: 5000000,
     m_label: "50",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 10000000,
+    im_label: "50",
+    im_scale: "SCALE.KT"
   },
   '22' : {
     m_value: 10000000,
     m_label: "100",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 20000000,
+    im_label: "100",
+    im_scale: "SCALE.KT"
   },
   '23' : {
     m_value: 20000000,
     m_label: "200",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 40000000,
+    im_label: "200",
+    im_scale: "SCALE.KT"
   },
   '24' : {
     m_value: 40000000,
     m_label: "400",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 80000000,
+    im_label: "400",
+    im_scale: "SCALE.KT"
   },
   '25' : {
     m_value: 80000000,
     m_label: "800",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 160000000,
+    im_label: "800",
+    im_scale: "SCALE.KT"
   },
   '26' : {
     m_value: 160000000,
     m_label: "1,600",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 320000000,
+    im_label: "1,600",
+    im_scale: "SCALE.KT"
   },
   '27' : {
     m_value: 320000000,
     m_label: "3,200",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 640000000,
+    im_label: "3,200",
+    im_scale: "SCALE.KT"
   },
   '28' : {
     m_value: 640000000,
     m_label: "6,400",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 1250000000,
+    im_label: "6,400",
+    im_scale: "SCALE.KT"
   },
   '29' : {
     m_value: 1250000000,
     m_label: "12,500",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 2500000000,
+    im_label: "12,500",
+    im_scale: "SCALE.KT"
   },
   '30' : {
     m_value: 2500000000,
     m_label: "25,000",
     m_scale: "SCALE.KT",
-    im_value: ,
-    im_label: "",
-    im_scale: "SCALE."
+    im_value: 5000000000,
+    im_label: "25,000",
+    im_scale: "SCALE.KT"
+  }
+}
+
+export const TIME = {
+  "-5": {
+    time_value: 0.125,
+    time_label: "1/8",
+    time_scale: "SCALE.Second"
+  },
+  "-4": {
+    time_value: 0.25,
+    time_label: "1/4",
+    time_scale: "SCALE.Second"
+  },
+  "-3": {
+    time_value: 0.5,
+    time_label: "1/2",
+    time_scale: "SCALE.Second"
+  },
+  "-2": {
+    time_value: 1,
+    time_label: "1",
+    time_scale: "SCALE.Second"
+  },
+  "-1": {
+    time_value: 3,
+    time_label: "3",
+    time_scale: "SCALE.Seconds"
+  },
+  "0": {
+    time_value: 6,
+    time_label: "6",
+    time_scale: "SCALE.Seconds"
+  },
+  "1": {
+    time_value: 12,
+    time_label: "12",
+    time_scale: "SCALE.Seconds"
+  },
+  "2": {
+    time_value: 30,
+    time_label: "30",
+    time_scale: "SCALE.Seconds"
+  },
+  "3": {
+    time_value: 60,
+    time_label: "1",
+    time_scale: "SCALE.Minute"
+  },
+  "4": {
+    time_value: 120,
+    time_label: "2",
+    time_scale: "SCALE.Minutes"
+  },
+  "5": {
+    time_value: 240,
+    time_label: "4",
+    time_scale: "SCALE.Minutes"
+  },
+  "6": {
+    time_value: 480,
+    time_label: "8",
+    time_scale: "SCALE.Minutes"
+  },
+  "7": {
+    time_value: 900,
+    time_label: "15",
+    time_scale: "SCALE.Minutes"
+  },
+  "8": {
+    time_value: 1800,
+    time_label: "30",
+    time_scale: "SCALE.Minutes"
+  },
+  "9": {
+    time_value: 3600,
+    time_label: "1",
+    time_scale: "SCALE.Hour"
+  },
+  "9": {
+    time_value: 7200,
+    time_label: "2",
+    time_scale: "SCALE.Hours"
+  },
+  "10": {
+    time_value: 14400,
+    time_label: "4",
+    time_scale: "SCALE.Hours"
+  },
+  "11": {
+    time_value: 28800,
+    time_label: "8",
+    time_scale: "SCALE.Hours"
+  },
+  "12": {
+    time_value: 57600,
+    time_label: "16",
+    time_scale: "SCALE.Hours"
+  },
+  "13": {
+    time_value: null,
+    time_label: "1",
+    time_scale: "SCALE.Day"
+  },
+  "14": {
+    time_value: null,
+    time_label: "2",
+    time_scale: "SCALE.Days"
+  },
+  "15": {
+    time_value: null,
+    time_label: "4",
+    time_scale: "SCALE.Days"
+  },
+  "16": {
+    time_value: null,
+    time_label: "1",
+    time_scale: "SCALE.Week"
+  },
+  "17": {
+    time_value: null,
+    time_label: "2",
+    time_scale: "SCALE.Weeks"
+  },
+  "18": {
+    time_value: null,
+    time_label: "1",
+    time_scale: "SCALE.Month"
+  },
+  "19": {
+    time_value: null,
+    time_label: "2",
+    time_scale: "SCALE.Months"
+  },
+  "20": {
+    time_value: null,
+    time_label: "4",
+    time_scale: "SCALE.Months"
+  },
+  "21": {
+    time_value: null,
+    time_label: "8",
+    time_scale: "SCALE.Months"
+  },
+  "22": {
+    time_value: null,
+    time_label: "1.5",
+    time_scale: "SCALE.Years"
+  },
+  "23": {
+    time_value: null,
+    time_label: "3",
+    time_scale: "SCALE.Years"
+  },
+  "24": {
+    time_value: null,
+    time_label: "6",
+    time_scale: "SCALE.Years"
+  },
+  "25": {
+    time_value: null,
+    time_label: "12",
+    time_scale: "SCALE.Years"
+  },
+  "26": {
+    time_value: null,
+    time_label: "25",
+    time_scale: "SCALE.Years"
+  },
+  "27": {
+    time_value: null,
+    time_label: "",
+    time_scale: "SCALE.Years"
+  },
+  "28": {
+    time_value: null,
+    time_label: "50",
+    time_scale: "SCALE.Years"
+  },
+  "29": {
+    time_value: null,
+    time_label: "100",
+    time_scale: "SCALE.Years"
+  },
+  "30": {
+    time_value: null,
+    time_label: "200",
+    time_scale: "SCALE.Years"
   }
 }

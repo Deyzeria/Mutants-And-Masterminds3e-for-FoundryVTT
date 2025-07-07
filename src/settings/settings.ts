72156b06b9556e9e64d6223ccf163fee85d331a6
Deyzeria <vikbar199@gmail.com>
2025-07-07 13:50:59 +0300

    Moved stuff around so that no invalid code is loaded

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,15 +1,14 @@
 import { SYSTEM } from "../config/system.ts";
-import { MovementConfig } from "./movement-settings.ts";
 
 export default function RegisterSystemSettings() {
-  game.settings?.registerMenu(SYSTEM.id, SYSTEM.SETTINGS.MOVEMENT, {
-      name: "SETTINGS.MOVEMENT.Name",
-      label: "SETTINGS.MOVEMENT.Label",
-      hint: "SETTINGS.MOVEMENT.Hint",
-      icon: "fa-solid fa-person-walking",
-      type: MovementConfig,
-      restricted: true
-  });
+  // game.settings?.registerMenu(SYSTEM.id, SYSTEM.SETTINGS.MOVEMENT, {
+  //     name: "SETTINGS.MOVEMENT.Name",
+  //     label: "SETTINGS.MOVEMENT.Label",
+  //     hint: "SETTINGS.MOVEMENT.Hint",
+  //     icon: "fa-solid fa-person-walking",
+  //     type: MovementConfig,
+  //     restricted: true
+  // });
   game.settings?.register(SYSTEM.id, SYSTEM.SETTINGS.MOVEMENT, {
     name: "SETTINGS.MOVEMENT.Name",
     scope: "world",
